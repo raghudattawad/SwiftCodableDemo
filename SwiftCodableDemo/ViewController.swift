bb06dd6f2516dce7efe1fb91b57ab808129f44a0
Raghavendra Dattawad <raghavendra.d@wickedride.com>
2018-10-15 00:47:34 -0700

    Initial Commit

---
+++ SwiftCodableDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftCodableDemo
+//
+//  Created by Raghavendra Dattawad on 10/15/18.
+//  Copyright © 2018 Raghavendra Dattawad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
